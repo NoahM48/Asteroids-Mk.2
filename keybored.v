e5742fc05d3c45f6a96c2e6f8d6a904c18e6c100
NoahM48 <58572465+NoahM48@users.noreply.github.com>
2019-12-13 02:45:59 -0500

    Update keybored.v

--- keybored.v
+++ keybored.v
@@ -1,12 +1,10 @@
-module keybored(KB_clk, key0, key1, key2, key3, direction, cont1, cont2, cont3);
+module keybored(KB_clk, key0, key1, key2, key3, direction);
 	input KB_clk;
 	input key0,key1,key2,key3;
-	input cont1, cont2, cont3;
 	output reg [2:0]direction;
 
 	always @(KB_clk)
-	begin
-	if (cont1 == 1'b1)
+	
 	begin // Beefender has options for multiple control schemes. I'm deciding whether to keep it or not.
 		if(key3 == 1'b1 & key2 == 1'b1 & key1 == 1'b0 & key0 == 1'b1)
 			direction = 3'b000;//left
@@ -17,30 +15,5 @@ module keybored(KB_clk, key0, key1, key2, key3, direction, cont1, cont2, cont3);
 		else if(key3 == 1'b1 & key2 == 1'b0 & key1 == 1'b1 & key0 == 1'b1)
 			direction = 3'b011;//down
 		else 	direction = 3'b100;//stationary
-	end
-	else if (cont2 == 1'b1)
-	begin
-		if(key3 == 1'b1 & key2 == 1'b1 & key1 == 1'b1 & key0 == 1'b0)
-			direction = 3'b000;//left
-		else if(key3 == 1'b1 & key2 == 1'b0 & key0 == 1'b1 & key1 == 1'b1)
-			direction = 3'b001;//right
-		else if(key2 == 1'b1 & key3 == 1'b0 & key1 == 1'b1 & key0 == 1'b1)
-			direction = 3'b010;//up
-		else if(key3 == 1'b1 & key2 == 1'b1 & key1 == 1'b0 & key0 == 1'b1)
-			direction = 3'b011;//down
-		else 	direction = 3'b100;//stationary
-	end
-	else if (cont3 == 1'b1)
-	begin
-		if(key3 == 1'b0 & key2 == 1'b1 & key1 == 1'b1 & key0 == 1'b1)
-			direction = 3'b000;//left
-		else if(key3 == 1'b1 & key2 == 1'b1 & key0 == 1'b1 & key1 == 1'b0)
-			direction = 3'b001;//right
-		else if(key2 == 1'b1 & key3 == 1'b1 & key1 == 1'b1 & key0 == 1'b0)
-			direction = 3'b010;//up
-		else if(key3 == 1'b1 & key2 == 1'b0 & key1 == 1'b1 & key0 == 1'b1)
-			direction = 3'b011;//down
-		else 	direction = 3'b100;//stationary
-	end
 	end
 endmodule
